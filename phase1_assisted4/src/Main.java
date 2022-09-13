0312ebe8f58479f4f0883ee598b81728e6009bd8
Vaishnavi <vaishnavirawat2902@gmail.com>
2022-09-13 18:18:57 +0530

    changes

---
+++ phase1_assisted4/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		rotateArray r = new rotateArray();
+		int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+		r.rotate(arr, 5);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+}
